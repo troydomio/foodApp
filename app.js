5227c5d4c1f18ec268871ae7b2f7b277d38f7a55
Alieu Baldeh <alieu.dev@gmail.com>
2021-07-12 22:33:58 -0400

    added styling with flexbox, added star rating

--- app.js
+++ app.js
@@ -25,7 +25,22 @@ function fetchData(query){
     // .then(data => renderMealsCard(data))
     // .catch(error => console.log('error', error));
 }
-
+function rating(){
+    return (
+        `<div class="rate">
+        <input type="radio" id="star5" name="rate" value="5" />
+        <label for="star5" title="text">5 stars</label>
+        <input type="radio" id="star4" name="rate" value="4" />
+        <label for="star4" title="text">4 stars</label>
+        <input type="radio" id="star3" name="rate" value="3" />
+        <label for="star3" title="text">3 stars</label>
+        <input type="radio" id="star2" name="rate" value="2" />
+        <label for="star2" title="text">2 stars</label>
+        <input type="radio" id="star1" name="rate" value="1" />
+        <label for="star1" title="text">1 star</label>
+    </div>`
+    )
+}
 function renderMealsCard(mealInfo){
     let divFrame = document.createElement('div')
     let title = document.createElement('h2')
@@ -33,7 +48,12 @@ function renderMealsCard(mealInfo){
     title.textContent = mealInfo.title
     img.src = mealInfo.image
 
+    let ratingStars = document.createElement('p')
+    ratingStars.innerHTML = rating()
+    
+
+
     let container = document.querySelector('#search-results')
-    divFrame.append(title, img)
+    divFrame.append(title, img, ratingStars)
     container.append(divFrame)
 }
