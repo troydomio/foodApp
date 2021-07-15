642dcae981e5d1141de30a0ae785afe096293f7c
Alieu Baldeh <alieu.dev@gmail.com>
2021-07-15 10:53:24 -0400

    added intial rendering functionality

--- app.js
+++ app.js
@@ -1,6 +1,137 @@
-const dotenv = require('dotenv').config();
-const API_KEY = process.env.API_KEY;
+// const dotenv = require('dotenv').config();
+// const API_KEY = process.env.API_KEY;
+const API_KEY = '2b744349f3d8403ab9076a2080cd086d'
 
+//////////////////////////////////////////////////////////////////////////////
+document.addEventListener('DOMContentLoaded', ()=> {
+    fetchInitialData()
+}) 
+
+ // this function fetches all of my data! Using troy.json
+    function fetchInitialData(){
+        fetch('http://localhost:3000/results')
+        .then(res=> res.json())
+        .then(data => {initialRender(data)})
+    }
+
+
+function initialRender(data){
+    //buttons and divs are created here 
+    let body = document.querySelector('body')
+    let buttonContainer = document.createElement('div')
+    buttonContainer.id = "button-container"
+    let buttonsContainer = document.createElement('div')
+    buttonsContainer.id= "buttons-go-here"
+    let buttonResultsContainer= document.createElement('div')
+    buttonResultsContainer.id = "button-results-container"
+    let h3RecipeTitle = document.createElement('h3')
+    let imgResult = document.createElement('img')
+     let h5Summary= document.createElement('h5')
+    
+    let quickMealButton = document.createElement('button')
+    quickMealButton.id = "quick-meals-button"
+    quickMealButton.textContent="Quick Meals"
+    let clear = document.createElement('button')
+    clear.id = "clear"
+    clear.textContent = "Clear"
+    let vegetarianMeals = document.createElement('button')
+    vegetarianMeals.id = "vegetarian-meals"
+    vegetarianMeals.textContent="Healthy Meals"
+    h3RecipeTitle.id="recipe-title"
+    
+    body.append(buttonContainer)
+    buttonsContainer.append(quickMealButton,vegetarianMeals, clear, buttonResultsContainer)
+    buttonContainer.append(buttonsContainer)
+    
+    
+   // button event listeners
+   quickMealButton.addEventListener('click', ()=> {
+         h3RecipeTitle.innerHTML=""        
+            data.forEach((meals)=>{ 
+                if(meals.readyInMinutes >= 30 ){
+                let recipeUl = document.createElement('ul')
+                let recipeLi = document.createElement('li')
+                let summaryLi = document.createElement('li')
+                let image = document.createElement('img')
+                let title = document.createElement('li')
+                let h4Time = document.createElement('li')
+                let liGetRecipeContainer = document.createElement('li')
+                let getRecipeDetails = document.createElement('details')
+                let getRecipeSummary = document.createElement('summary')
+                let getRecipeMoreDetails = document.createElement('p')  
+
+                getRecipeDetails.id="details"
+                getRecipeSummary.textContent= "Get the recipe"
+                getRecipeDetails.append(getRecipeSummary, getRecipeMoreDetails)
+                liGetRecipeContainer.append(getRecipeDetails)
+                h4Time.textContent = `Ready in: ${meals.readyInMinutes} minutes`
+                image.src = meals.image
+                title.textContent = meals.title
+                summaryLi.innerHTML = meals.summary
+                recipeUl.appendChild(recipeLi)
+                recipeLi.append(title, image, summaryLi, h4Time,liGetRecipeContainer)
+                h3RecipeTitle.append(recipeUl)
+                buttonResultsContainer.append(h3RecipeTitle)
+
+                let steps= meals.analyzedInstructions[0].steps
+                    steps.forEach((stepList)=>{
+                        let step = document.createElement('p')
+                        getRecipeMoreDetails.append(step)
+                        let stepNumber = stepList.number
+                        let direction = stepList.step
+                        step.textContent=`Step${stepNumber}: ${direction}`
+                    })
+            } }) })
+                    
+    vegetarianMeals.addEventListener('click', ()=> {
+        h3RecipeTitle.innerHTML=""        
+            data.forEach((meals)=>{ 
+                 if(meals.healthScore > 30 ){          
+                let recipeUl = document.createElement('ul')
+                    let recipeLi = document.createElement('li')
+                    let summaryLi = document.createElement('li')
+                    let image = document.createElement('img')
+                    let title = document.createElement('li')
+                    let h4Time = document.createElement('li')
+                    let liGetRecipeContainer = document.createElement('li')
+                    let getRecipeDetails = document.createElement('details')
+                    let getRecipeSummary = document.createElement('summary')
+                    let getRecipeMoreDetails = document.createElement('p')
+                    
+                    getRecipeDetails.id="details"
+                    getRecipeSummary.textContent= "Get the recipe"
+                    getRecipeDetails.append(getRecipeSummary, getRecipeMoreDetails)
+                    liGetRecipeContainer.append(getRecipeDetails)
+                    h4Time.textContent = `Ready in: ${meals.readyInMinutes} minutes`
+                    image.src = meals.image
+                    title.textContent = meals.title
+                    summaryLi.innerHTML = meals.summary
+                    recipeUl.appendChild(recipeLi)
+                    recipeLi.append(title, image, summaryLi, h4Time,liGetRecipeContainer)
+                    h3RecipeTitle.append(recipeUl)
+                    buttonResultsContainer.append(h3RecipeTitle)
+
+                         let steps= meals.analyzedInstructions[0].steps
+                        steps.forEach((stepList)=>{
+                            let step = document.createElement('p')
+                            getRecipeMoreDetails.append(step)
+                            let stepNumber = stepList.number
+                            let direction = stepList.step
+                            step.textContent=`Step${stepNumber}: ${direction}`
+                        })
+                        } }) })    
+                    
+        clear.addEventListener('click',()=>{
+        let test = document.createElement('h1')
+        buttonResultsContainer.innerHTML=''
+        test.textContent=""
+        buttonResultsContainer.append(test)             
+                         })                      
+
+                }
+
+
+// Search funtionality 
 const form = document.querySelector('#search-form')
 form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -20,7 +151,7 @@ function fetchData(query){
         .catch(error => console.log('error', error));
     
 }
-function renderRecipe(recipe, modal){
+function renderRecipe(recipe){
     let id = recipe.id 
 
     let requestOptions = {
@@ -85,18 +216,44 @@ function renderMealsCard(mealInfo){
     title.textContent = mealInfo.title
     img.src = mealInfo.image
 
-    // review stars
-    let ratingStars = document.createElement('p')
-    ratingStars.innerHTML = rating()
+    // // review stars
+    // let ratingStars = document.createElement('p')
+    // ratingStars.innerHTML = rating()
 
     // modal
     let myModal = document.createElement('div')
     myModal.innerHTML = createModal()
 
-    //user ratings-- will only work with mock json data
-    let avgRating = mealInfo.userRating
+    //user ratings
+    let totalRating = 10
+    let numberRatings = 2
+    let avgRating = totalRating/numberRatings
     let pRating = document.createElement('p')
     pRating.textContent = `User Rating: ${avgRating}`
+    //ratings stars
+    let ratingStars = document.createElement('p')
+    ratingStars.innerHTML = rating()
+    //make star rating button change the user ratings average when clicked
+    ratingStars.addEventListener("click", (e) => {
+        if (e.target.value === "5") {
+            totalRating +=5 
+            numberRatings++
+        } else if (e.target.value === "4") {
+            totalRating +=4
+            numberRatings++
+        } else if (e.target.value === "3") {
+            totalRating +=3
+            numberRatings++
+        } else if (e.target.value === "2") {
+            totalRating +=2 
+            numberRatings++
+        } else if (e.target.value === "1") {
+            totalRating ++ 
+            numberRatings++
+        }
+        avgRating = (totalRating/numberRatings).toFixed(2)
+        pRating.textContent = `User Rating: ${avgRating}`
+    })
 
     //review form
     let reviewForm = document.createElement("form");
